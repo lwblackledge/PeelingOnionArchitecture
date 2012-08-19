abb839ab64ea2955354a76fe45591c8ea8305941
lwblackledge <lwblackledge@gmail.com>
2012-08-18 21:12:47 -0400

    service locator

---
+++ PeelingOnionArchitecture.Core/ServiceLocator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeelingOnionArchitecture.Core
+{
+    public interface IServiceLocator
+    {
+        T GetInstance<T>();
+        object GetInstance(Type type);
+    }
+
+    public static class ServiceLocator
+    {
+        private static IServiceLocator _current;
+        public static IServiceLocator Current
+        {
+            get
+            {
+                return _current;
+            }
+        }
+
+        public static void SetServiceLocator(Func<IServiceLocator> create)
+        {
+            if (create == null)
+            {
+                throw new ArgumentNullException("create");
+            }
+            _current = create.Invoke();
+        }
+    }
+}
---
+++ PeelingOnionArchitecture.Resolver/SimpleInjectorServiceLocator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PeelingOnionArchitecture.Core;
+
+namespace PeelingOnionArchitecture.Resolver
+{
+    public class SimpleInjectorServiceLocator : IServiceLocator
+    {
+        public T GetInstance<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetInstance(Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
